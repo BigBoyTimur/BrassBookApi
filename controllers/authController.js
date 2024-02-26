bc7d47b820110fceaf08db1f314f712dfac074aa
Shilin Timur Sergeevich <roadyleet@gmail.com>
2024-02-26 15:36:58 +0300

    refresh

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,10 +2,12 @@ import {User} from "../models/User.js";
 import {comparePassword, generateAccessToken, generateRefreshToken, hashPassword} from "../helpers/auth.js";
 import * as crypto from "crypto";
 import {codeSend} from "../helpers/mail.js";
+import jwt from "jsonwebtoken";
+import 'dotenv/config'
 
 
 export const registerUser = async (req, res) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body;
 
   if (!email) {
     res.status(400);
@@ -39,7 +41,7 @@ export const registerUser = async (req, res) => {
   return res.json({id: user.dataValues.user_id});
 }
 
-export const checkMailCode = async (req, res) => {
+export const verifyUser = async (req, res) => {
   const { email, code } = req.body;
   const user = await User.findOne({ where: { email: email } });
   if ((Date.now() - user.code_date) / 1000 > 15 * 60) {
@@ -53,7 +55,7 @@ export const checkMailCode = async (req, res) => {
   }
 
   res.status(400);
-  return res.json({error: "Неправильный код"});
+  return res.json({error: 'Неправильный код'});
 }
 
 export const sendCode = async (req, res) => {
@@ -99,5 +101,42 @@ export const loginUser = async (req, res) => {
   const accessToken = await generateAccessToken(user.dataValues.user_id, email);
   const refreshToken = await generateRefreshToken(user.dataValues.user_id, email);
   res.cookie('refreshToken', refreshToken, {maxAge: 60 * 24 * 60 * 60 * 1000, httpOnly: true });
-  res.json({'access_token': accessToken, 'refresh_token': refreshToken});
+  return res.json({'access_token': accessToken, 'refresh_token': refreshToken});
+}
+
+export const refreshToken = async (req, res) => {
+  let user;
+  if (!req.cookies.refreshToken){
+    res.status(400);
+    return res.json({error: 'в куках нет рефреш токена'});
+  }
+  await jwt.verify(req.cookies.refreshToken, process.env.JWT_SECRET, async (err, decoded) => {
+    if (err) {
+      res.status(400);
+      return res.json({error: 'токен не валиден, необходимо войти еще раз'});
+    }
+    user = await User.findOne({ where: { user_id: +decoded.user_id } });
+  })
+  const accessToken = await generateAccessToken(user.dataValues.user_id, user.dataValues.email);
+  const refreshToken = await generateRefreshToken(user.dataValues.user_id, user.dataValues.email);
+  res.cookie('refreshToken', refreshToken, {maxAge: 60 * 24 * 60 * 60 * 1000, httpOnly: true });
+  return res.json({'access_token': accessToken, 'refresh_token': refreshToken});
+}
+
+
+export const passwordUpdate = async (req, res) => {
+  const { email, code, newPassword } = req.body;
+  const user = await User.findOne({ where: { email: email } });
+  if ((Date.now() - user.code_date) / 1000 > 15 * 60) {
+    res.status(400);
+    return res.json({error: 'код устарел'});
+  }
+  if (code !== user.code) {
+    res.status(400)
+    return res.json({error: 'Неправильный код'})
+  }
+  console.log(newPassword)
+  user.password = await hashPassword(newPassword)
+  user.save();
+  return res.sendStatus(200)
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
-import {loginUser, registerUser, checkMailCode, sendCode} from "../controllers/authController.js";
+import {loginUser, registerUser, verifyUser, sendCode, refreshToken, passwordUpdate} from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
 authRouter.post('/registration', registerUser);
 authRouter.post('/login', loginUser);
-authRouter.put('/verifyuser', checkMailCode);
+authRouter.put('/verifyuser', verifyUser);
 authRouter.put('/sendcode', sendCode);
+authRouter.get('/refresh', refreshToken);
+authRouter.put('/registration', passwordUpdate)
 
 
 export { authRouter }
